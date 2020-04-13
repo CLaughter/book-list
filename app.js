f51cc6fcbeaaf843ad6fd124679a9801c43955f2
Chris Laughter <chrislaugh@gmail.com>
2020-04-12 18:41:14 -0600

    validation and alert

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ function Book(title, author, isbn) {
 function UIadd() {}
 function UIread() {}
 
-// Add book to Add list
-UIadd.prototype.addBookToAddList = function (bookAdd) {
+// Add book to Add or Read list
+const bookListAdd = function (bookAdd) {
   const addList = document.getElementById("addBook-list");
   // Create tr element
   const row = document.createElement("tr");
@@ -25,17 +25,10 @@ UIadd.prototype.addBookToAddList = function (bookAdd) {
 
   addList.appendChild(row);
 };
+UIadd.prototype.addBookToAddList = bookListAdd;
 
-// Clear fields
-UIadd.prototype.clearFields = function () {
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("isbn").value = "";
-};
-
-// Add book to Read list
-UIread.prototype.addBookToReadList = function (bookRead) {
-  const readList = document.getElementById("readBook-list");
+const bookListRead = function (bookRead) {
+  const addList = document.getElementById("readBook-list");
   // Create tr element
   const row = document.createElement("tr");
   // Insert cols
@@ -46,15 +39,43 @@ UIread.prototype.addBookToReadList = function (bookRead) {
   <td><a href="#" class="delete">X<a></td>
   `;
 
-  readList.appendChild(row);
+  addList.appendChild(row);
+};
+UIread.prototype.addBookToReadList = bookListRead;
+
+// Display Alert
+const alert = function (message, className) {
+  // Create div
+  const div = document.createElement("div");
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add textnode and text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector(".card");
+  // Get form
+  const form = document.querySelector("#book-form");
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
 };
 
+UIadd.prototype.showAlert = alert;
+UIread.prototype.showAlert = alert;
+
 // Clear fields
-UIread.prototype.clearFields = function () {
+let clear = function () {
   document.getElementById("title").value = "";
   document.getElementById("author").value = "";
   document.getElementById("isbn").value = "";
 };
+
+UIadd.prototype.clearFields = clear;
+UIread.prototype.clearFields = clear;
 
 // everything else goes in the prototype
 // Event Listeners
@@ -70,11 +91,19 @@ document.getElementById("addBook").addEventListener("click", function (e) {
   // Instantiate UI
   const uiAdd = new UIadd();
 
-  //  Add book to Add list
-  uiAdd.addBookToAddList(book);
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    uiAdd.showAlert("Please fill in all fields", "error");
+  } else {
+    //  Add book to Read list
+    uiAdd.addBookToAddList(book);
 
-  // CLear fields
-  uiAdd.clearFields();
+    // Alert success
+    uiAdd.showAlert("Book Added", "success");
+
+    // CLear fields
+    uiAdd.clearFields();
+  }
 
   e.preventDefault();
 });
@@ -91,11 +120,19 @@ document.getElementById("readBook").addEventListener("click", function (e) {
   // Instantiate UI
   const uiRead = new UIread();
 
-  //  Add book to Read list
-  uiRead.addBookToReadList(book);
+  // Validate
+  if (title === "" || author === "") {
+    uiRead.showAlert("Please fill in all fields", "error");
+  } else {
+    //  Add book to Read list
+    uiRead.addBookToReadList(book);
 
-  // CLear fields
-  uiRead.clearFields();
+    // Alert success
+    uiRead.showAlert("Book Added", "success");
+
+    // CLear fields
+    uiRead.clearFields();
+  }
 
   e.preventDefault();
 });
